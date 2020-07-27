7ce38e7b90fff30cd749021ee42cd78fee3b96af
Sumtudou <1163015195@qq.com>
2020-07-27 15:13:58 +0800

    [WFGames][Jay] refactor:use Map<String,int> replace Map<String,Input> and remove unuse codes

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -10,54 +10,32 @@ import java.time.LocalDateTime;
 public class WordFrequencyGame {
     public String getResult(String inputStr) {
 
+        String[] words = inputStr.split("\\s+");
+        Map<String, Integer> wordTimesMap = new HashMap<>();
 
-
-
-
-                //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split("\\s+");
-
-                List<Input> inputList = new ArrayList<>();
-                for (String s : arr) {
-                    Input input = new Input(s, 1);
-                    inputList.add(input);
-                }
-
-                //get the map for the next step of sizing the same word
-                Map<String, List<Input>> map =getListMap(inputList);
-
-                List<Input> list = new ArrayList<>();
-                for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
-                    Input input = new Input(entry.getKey(), entry.getValue().size());
-                    list.add(input);
-                }
-                inputList = list;
-
-                inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
-                StringJoiner joiner = new StringJoiner("\n");
-                for (Input w : inputList) {
-                    String s = w.getValue() + " " +w.getWordCount();
-                    joiner.add(s);
-                }
-                return joiner.toString();
-
-
-    }
-
-    private Map<String, List<Input>> getListMap(List<Input> inputList) {
-        Map<String, List<Input>> map = new HashMap<>();
-        for (Input input : inputList){
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(input.getValue())) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(input);
-                map.put(input.getValue(), arr);
-            }
-            else {
-                map.get(input.getValue()).add(input);
+        for (String word : words) {
+            if (!wordTimesMap.containsKey(word)) {
+                wordTimesMap.put(word, 1);
+            } else {
+                wordTimesMap.put(word, wordTimesMap.get(word) + 1);
             }
         }
-        return map;
+
+        List<Input> wordTimesList = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : wordTimesMap.entrySet()) {
+            Input input = new Input(entry.getKey(), entry.getValue());
+            wordTimesList.add(input);
+        }
+
+        wordTimesList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+
+        StringJoiner joiner = new StringJoiner("\n");
+        for (Input w : wordTimesList) {
+            String s = w.getValue() + " " + w.getWordCount();
+            joiner.add(s);
+        }
+        return joiner.toString();
+
     }
+
 }
