1e78a8344d6e098bf77a6879c8da034182584d48
Sumtudou <1163015195@qq.com>
2020-07-27 15:20:09 +0800

    [WFGames][Jay] refactor:extract 3 operate method from WordFrequencyGame

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -11,8 +11,33 @@ public class WordFrequencyGame {
     public String getResult(String inputStr) {
 
         String[] words = inputStr.split("\\s+");
-        Map<String, Integer> wordTimesMap = new HashMap<>();
+        Map<String, Integer> wordTimesMap = getWordTimesMap(words);
+        List<Input> wordTimesList = getWordTimesList(wordTimesMap);
+        return getResult(wordTimesList);
+    }
 
+    private String getResult(List<Input> wordTimesList) {
+
+        StringJoiner joiner = new StringJoiner("\n");
+        for (Input wordTimes : wordTimesList) {
+            String wordTimesStr = wordTimes.getValue() + " " + wordTimes.getWordCount();
+            joiner.add(wordTimesStr);
+        }
+        return joiner.toString();
+    }
+
+    private List<Input> getWordTimesList(Map<String, Integer> wordTimesMap) {
+        List<Input> wordTimesList = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : wordTimesMap.entrySet()) {
+            Input input = new Input(entry.getKey(), entry.getValue());
+            wordTimesList.add(input);
+        }
+        wordTimesList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+        return wordTimesList;
+    }
+
+    private Map<String, Integer> getWordTimesMap(String[] words) {
+        Map<String, Integer> wordTimesMap = new HashMap<>();
         for (String word : words) {
             if (!wordTimesMap.containsKey(word)) {
                 wordTimesMap.put(word, 1);
@@ -20,22 +45,7 @@ public class WordFrequencyGame {
                 wordTimesMap.put(word, wordTimesMap.get(word) + 1);
             }
         }
-
-        List<Input> wordTimesList = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : wordTimesMap.entrySet()) {
-            Input input = new Input(entry.getKey(), entry.getValue());
-            wordTimesList.add(input);
-        }
-
-        wordTimesList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
-        StringJoiner joiner = new StringJoiner("\n");
-        for (Input w : wordTimesList) {
-            String s = w.getValue() + " " + w.getWordCount();
-            joiner.add(s);
-        }
-        return joiner.toString();
-
+        return wordTimesMap;
     }
 
 }
